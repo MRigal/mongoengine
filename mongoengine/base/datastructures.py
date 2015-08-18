e696ff1abc627251d0173d026f1efed23b3f1c51
Matthieu Rigal <matt.rigal@gmail.com>
2015-08-18 19:02:54 +0200

    Test getitem on BaseList/BaseDict

--- mongoengine/base/datastructures.py
+++ mongoengine/base/datastructures.py
@@ -29,14 +29,14 @@ class BaseDict(dict):
         EmbeddedDocument = _import_class('EmbeddedDocument')
         if isinstance(value, EmbeddedDocument) and value._instance is None:
             value._instance = self._instance
-        elif not isinstance(value, BaseDict) and isinstance(value, dict):
-            value = BaseDict(value, None, '%s.%s' % (self._name, key))
-            super(BaseDict, self).__setitem__(key, value)
-            value._instance = self._instance
-        elif not isinstance(value, BaseList) and isinstance(value, list):
-            value = BaseList(value, None, '%s.%s' % (self._name, key))
-            super(BaseDict, self).__setitem__(key, value)
-            value._instance = self._instance
+        # elif not isinstance(value, BaseDict) and isinstance(value, dict):
+        #     value = BaseDict(value, None, '%s.%s' % (self._name, key))
+        #     super(BaseDict, self).__setitem__(key, value)
+        #     value._instance = self._instance
+        # elif not isinstance(value, BaseList) and isinstance(value, list):
+        #     value = BaseList(value, None, '%s.%s' % (self._name, key))
+        #     super(BaseDict, self).__setitem__(key, value)
+        #     value._instance = self._instance
         return value
 
     def __setitem__(self, key, value, *args, **kwargs):
@@ -115,14 +115,14 @@ class BaseList(list):
         EmbeddedDocument = _import_class('EmbeddedDocument')
         if isinstance(value, EmbeddedDocument) and value._instance is None:
             value._instance = self._instance
-        elif not isinstance(value, BaseDict) and isinstance(value, dict):
-            value = BaseDict(value, None, '%s.%s' % (self._name, key))
-            super(BaseList, self).__setitem__(key, value)
-            value._instance = self._instance
-        elif not isinstance(value, BaseList) and isinstance(value, list):
-            value = BaseList(value, None, '%s.%s' % (self._name, key))
-            super(BaseList, self).__setitem__(key, value)
-            value._instance = self._instance
+        # elif not isinstance(value, BaseDict) and isinstance(value, dict):
+        #     value = BaseDict(value, None, '%s.%s' % (self._name, key))
+        #     super(BaseList, self).__setitem__(key, value)
+        #     value._instance = self._instance
+        # elif not isinstance(value, BaseList) and isinstance(value, list):
+        #     value = BaseList(value, None, '%s.%s' % (self._name, key))
+        #     super(BaseList, self).__setitem__(key, value)
+        #     value._instance = self._instance
         return value
 
     def __iter__(self):
